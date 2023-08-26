efbadd8d3eb987a8c567db8af1411e9b4b8d6873
Lakshmi Soragaon <lakshmisoragaon@gmail.com>
2023-08-26 15:12:40 +0530

    Modified disk_usage with render templates

--- disk_usage.py
+++ disk_usage.py
@@ -1,5 +1,5 @@
 import psutil
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
@@ -7,10 +7,14 @@ app = Flask(__name__)
 def index():
     drive = psutil.disk_usage('/')
     total = drive.total
+    total = total / 1000000000
     used = drive.used
+    used = used / 1000000000
     free = drive.free
+    free = free / 1000000000
     percent = drive.percent
-    return f"Total : {total} Used : {used} Free : {free} Percent : {percent}"
+    #return f"Total : {total} Used : {used} Free : {free} Percent : {percent}"
+    return render_template("index.html",total=total,used=used,free=free)
 
 if __name__=='__main__':
     app.run(debug=True, host = '0.0.0.0')
